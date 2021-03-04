768f812a1d08832d4597dcdb065a9dc2eedf1f43
vamsichalamkoti2718 <77767799+vamsichalamkoti2718@users.noreply.github.com>
2021-03-04 16:03:11 +0530

    Fixed Bug

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -7,7 +7,7 @@ import training.taylor.timetracker.core.dao.TimeEntry;
 import java.util.List;
 
 @Component
-public class Tracker 
+public class Tracker {
     @Autowired
     private List<TimeEntry> entries;
 
